82203d2cbc1500a166688c0603f3f8802bf11c8d
Piero Morales <piero.morales@utec.edu.pe>
2020-11-21 16:09:56 -0500

    Recursión

--- main.cpp
+++ main.cpp
@@ -24,24 +24,48 @@ class QuadTree{
 private:
     Node* root;
 
-    bool isUniqueColor(Node* &qNode){}
+    static bool isUniqueColor(Node* &qNode, CImg <char> &R){
+        char color = R(qNode->quad.first.first,qNode->quad.first.second);
+        for (int i = qNode->quad.first.first; i <= qNode->quad.second.first; i++) {
+            for (int j = qNode->quad.first.second; j <= qNode->quad.second.second; j++) {
+                if (R(i,j) != color) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
-    void insertRecursive(Node* &qNode){
-        if(isUniqueColor(qNode)){
+    void insertRecursive(Node* &qNode, CImg <char> &R){
+        if(isUniqueColor(qNode, R)){
+            qNode->color = R(qNode->quad.first.first, qNode->quad.first.second) != '0';
             return;
         }
-        qNode->m_pSon[0] = new Node({qNode->quad.first});
+        int midX = (qNode->quad.second.first-qNode->quad.first.first)/2;
+        int midY = (qNode->quad.second.second - qNode->quad.first.second)/2;
+        qNode->m_pSon[0] = new Node({qNode->quad.first,{qNode->quad.first.first+midX,qNode->quad.first.second+midY}});
+        qNode->m_pSon[1] = new Node({qNode->quad.first.first+midX+1,qNode->quad.first.second},{qNode->quad.second.first,qNode->quad.first.second+midY});
+        qNode->m_pSon[2] = new Node({qNode->quad.first.first,qNode->quad.first.second+midY+1},{qNode->quad.first.first+midX,qNode->quad.second.second});
+        qNode->m_pSon[3] = new Node({qNode->quad.first.first+midX+1,qNode->quad.first.second+midY+1},qNode->quad.second);
+        insertRecursive(qNode->m_pSon[0],R);
+        insertRecursive(qNode->m_pSon[1],R);
+        insertRecursive(qNode->m_pSon[2],R);
+        insertRecursive(qNode->m_pSon[3],R);
     }
 
 public:
     QuadTree() : root{nullptr} {};
-    void loadImage(CImg <char> &R){
-        auto q0Node = new Node({0,0},{R.width()/2, R.height()/2});
-        auto q1Node = new Node({R.width()/2+1,0},{R.width(), R.height()/2});
-        auto q2Node = new Node({0,R.height()/2 + 1},{R.width()/2, R.height()});
-        auto q3Node = new Node({R.width()/2 + 1,R.height() + 1},{R.width(), R.height()});
-    }
 
+    void loadImage(CImg <char> &R) {
+        this->root->m_pSon[0] = new Node({0,0},{R.width()/2, R.height()/2});
+        this->root->m_pSon[1] = new Node({R.width()/2+1,0},{R.width(), R.height()/2});
+        this->root->m_pSon[2] = new Node({0,R.height()/2 + 1},{R.width()/2, R.height()});
+        this->root->m_pSon[3] = new Node({R.width()/2 + 1,R.height() + 1},{R.width(), R.height()});
+        insertRecursive(this->root->m_pSon[0],R);
+        insertRecursive(this->root->m_pSon[1],R);
+        insertRecursive(this->root->m_pSon[2],R);
+        insertRecursive(this->root->m_pSon[3],R);
+    }
 };
 
 CImg <char> Binarizar(CImg<float> & img, int umbral) {
@@ -60,8 +84,6 @@ CImg <char> Binarizar(CImg<float> & img, int umbral) {
     }
     return R;
 }
-
-
 
 int main() {
     CImg <float> A("../imagen.jpeg");
